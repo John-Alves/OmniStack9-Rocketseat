f8be8e0bc25781943b99406871305efb92342498
John-Alves <johnnydealves99@gmail.com>
2019-10-04 15:29:06 -0300

    Aula 02 concluida

---
+++ backend/src/config/upload.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+const path = require('path');
+
+module.exports = {
+    storage: multer.diskStorage({
+        destination: path.resolve(__dirname, '..', '..', 'uploads'),
+        filename: (req, file, cb) => {
+            const exe = path.extname(file.originalname);
+            const name = path.basename(file.originalname, exe);
+
+            cb(null, `${name}-${Date.now()}${path.extname(exe)}`)
+        }
+    })
+}
--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -3,7 +3,12 @@ const User = require('../models/User');
 module.exports = {
     async store(req, res){
         const { email } = req.body; // O nome desse tipo de "extração" é desestruturação
-        const user = await User.create({ email });
+        
+        let user = await User.findOne({ email: email });
+
+        if (!user){
+            user = await User.create({ email });
+        }
         
         return res.json(user);
     }
